8441cced7f3d0ac6f9b8c78a985d014506c7cd79
Ubuntu <lincoln.harris@czbiohub.org>
2019-04-30 17:21:18 +0000

    maybe working?

--- run_mutational_patterns.R
+++ run_mutational_patterns.R
@@ -17,19 +17,23 @@ ref_genome <- "BSgenome.Hsapiens.UCSC.hg38"
 library(ref_genome, character.only = TRUE)
 
 # list some sample vcfs
-vcf_files <- list.files(path = './laud_samples_all', pattern = ".vcf", full.names = TRUE)
+vcf_files <- list.files(path = './laud_samples_gFilter', pattern = ".vcf", full.names = TRUE)
 
 # get cell names
-cell_names <- stringr::str_remove_all(vcf_files, ".vcf")
-cell_names <- stringr::str_remove_all(cell_names, "./laud_samples_all/")
+cell_names <- stringr::str_remove_all(vcf_files, "_unique.vcf")
+cell_names <- stringr::str_remove_all(cell_names, "./laud_samples_gFilter/")
 
 # load vcfs, as Grange objs
-vcfs <- read_vcfs_as_granges(vcf_files, cell_names, ref_genome)
+vcfs <- read_vcfs_as_granges(vcf_files, cell_names, ref_genome, check_alleles = TRUE)
+print(' ')
+print('finished loading vcfs')
+print(' ')
 
 # get type of each mut
 type_occurrences <- mut_type_occurrences(vcfs, ref_genome)
 
 # plot, and save
-pdf("mut_type_laud_all.pdf", width=4, height=4)
+pdf("mut_type_laud_nonFilter.pdf", width=4, height=4)
 plot_spectrum(type_occurrences)
 dev.off()
+
