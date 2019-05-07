897a71758cb1708c36e72b00c045bfd4a526b396
Ubuntu <ubuntu@ip-172-31-33-7.us-west-2.compute.internal>
2019-05-07 23:21:13 +0000

    maybe i can hack the plotting function?

--- run_mutational_patterns.R
+++ run_mutational_patterns.R
@@ -3,6 +3,9 @@
 #  install.packages("BiocManager")
 #BiocManager::install("MutationalPatterns")
 
+library(devtools)
+devtools::install_github("UMCUGenetics/MutationalPatterns", ref = "lincoln-harris")
+
 # load libraries
 library(MutationalPatterns)
 library(BSgenome)
@@ -17,11 +20,11 @@ ref_genome <- "BSgenome.Hsapiens.UCSC.hg38"
 library(ref_genome, character.only = TRUE)
 
 # list some sample vcfs
-vcf_files <- list.files(path = './samples_filter_all', pattern = ".vcf", full.names = TRUE)
+vcf_files <- list.files(path = './samples_filter_sub', pattern = ".vcf", full.names = TRUE)
 
 # get cell names
 cell_names <- stringr::str_remove_all(vcf_files, ".vcf")
-cell_names <- stringr::str_remove_all(cell_names, "./samples_filter_all/")
+cell_names <- stringr::str_remove_all(cell_names, "./samples_filter_sub/")
 
 # load vcfs, as Grange objs
 vcfs <- read_vcfs_as_granges(vcf_files, cell_names, ref_genome, check_alleles = TRUE)
@@ -33,7 +36,7 @@ print(' ')
 type_occurrences <- mut_type_occurrences(vcfs, ref_genome)
 
 # plot, and save
-pdf("mut_type_laud_gFilter.pdf", width=4, height=4)
+pdf("mut_type_laud_gFilter_sub1.pdf", width=4, height=4)
 plot_spectrum(type_occurrences)
 dev.off()
 
